06fc96943b9220823f39321d2cbc8d3cb0bd732f
dmytro <dmytro@greenbookdata.com>
2023-12-04 17:19:53 +0000

    create homepage

---
+++ resources/js/Pages/Homepage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+import { Link } from "@inertiajs/vue3"
+
+defineProps({ user: Object })
+</script>
+
+<template>
+        <Head title="Welcome" />
+        <h1>Welcome</h1>
+        <p>Coding challenge</p>
+    <Link href="/product-list">Products sort list</Link>
+    <br>
+    <Link href="/categories/create">Create category</Link>
+    <br>
+    <Link href="/products">Products</Link>
+    <br>
+</template>
