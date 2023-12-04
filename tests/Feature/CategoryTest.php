f04a96defdbc9663a49b321e066fd76a5996c861
dmytro <dmytro@greenbookdata.com>
2023-12-04 17:05:51 +0000

    Prepare the category test with model usage

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -2,15 +2,25 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_list_categories(): void
     {
+        Category::factory()->create();
         $response = $this->get('/categories');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)->assertJsonStructure([
+            '*' => [
+                'title',
+                'id'
+            ]
+        ]);
     }
 
     public function test_create_category(): void
@@ -22,8 +32,11 @@ class CategoryTest extends TestCase
 
     public function test_insert_category(): void
     {
-        $response = $this->post('/categories');
+        $category = Category::factory()->make();
+        $this->assertDatabaseCount('categories', 0);
+        $response = $this->post('/categories', $category->getAttributes());
+        $this->assertDatabaseCount('categories', 1);
 
-        $response->assertStatus(201);
+        $response->assertStatus(302)->assertRedirect('/');
     }
 }
